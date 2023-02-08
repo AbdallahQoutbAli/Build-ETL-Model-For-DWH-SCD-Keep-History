2a2563f291630996e5be237937228f90a8de02cf
Abdallah Ali <47276503+abdallahkotb5566@users.noreply.github.com>
2023-02-08 09:59:36 +0300

    Adding Scripts

---
+++ DATALAKE SCRIPT/City_DATALAKE.sql
@@ -0,0 +1,4 @@
+CREATE TABLE [dbo].[CITY](
+	[CITY_NAME] [nvarchar](50) NULL,
+	[CITY_KEY] [int] NULL
+) 
---
+++ DWH SCRIPT/CITY.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE [dbo].[CITY](
+	[CITY_SK] [int] IDENTITY(1,1) NOT NULL,
+	[CITY_NAME] [nvarchar](50) NULL,
+	[CITY_KEY] [int] NULL,
+	[Status] [char](1) NULL,
+	[INSERT_DATE] [datetime] NULL,
+	[END_DATE] [datetime] NULL
+) 
+
